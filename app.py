3faa16d0f13371aac6c5adf7e8e4af87f5f89690
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-11-06 18:28:18 -0300

    feat: int to code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 
 if opcao_escolhida == 1:
     print('Cadastrar restaurantes')
